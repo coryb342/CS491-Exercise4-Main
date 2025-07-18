e078792527e666f24f70f686b064bbf54003ea48
Cory Bateman <coryb342@gmail.com>
2025-07-18 16:46:15 -0700

    Add ability to get or put a single player attribute

--- public/playermodule.js
+++ public/playermodule.js
@@ -27,3 +27,32 @@ export async function putPlayerData(player_id, player) {
       throw error;
   }
 }
+
+export async function getPlayerAttribute(player_id, attribute) {
+  try {
+    const player = await getPlayerData(player_id);
+    if (player && attribute in player) {
+      return player[attribute];
+    } else {
+      throw new Error(`Attribute ${attribute} not found for player ${player_id}.`);
+    }
+  } catch (error) {
+    console.error('Error getting player attribute:', error);
+    throw error;
+  }
+}
+
+export async function putPlayerAttribute(player_id, attribute, value) {
+  try {
+    const player = await getPlayerData(player_id);
+    if (player && attribute in player) {
+      player[attribute] = value;
+      return await putPlayerData(player_id, player);
+    } else {
+      throw new Error(`Attribute ${attribute} not found for player ${player_id}.`);
+    }
+  } catch (error) {
+    console.error('Error updating player attribute:', error);
+    throw error;
+  }
+}
