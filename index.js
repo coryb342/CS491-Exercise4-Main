84f2d294f2284ba23df07e9b97d0293771bacce1
Cory Bateman <coryb342@gmail.com>
2025-07-18 16:57:39 -0700

    Add endpoints for gamestate and coin data

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+//Player Endpoints
 app.get('/player/:id', (req, res) => {
   const player_id = req.params.id; // e.g., "1", "2", etc.
   const file_path = path.join(__dirname, `player_${player_id}.json`);
@@ -30,6 +31,50 @@ app.put('/player/:id', (req, res) => {
     res.status(200).send('Player data updated successfully');
   });
 })
+
+//Coin Endpoints
+app.get('/coin', (req, res) => {
+  const file_path = path.join(__dirname, 'coin.json');
+  res.sendFile(file_path, err => {
+    if (err) {
+      res.status(404).send('Coin file not found');
+    }
+  });
+});
+
+app.put('/coin', (req, res) => {
+  const updated_coin_data = req.body;
+  const file_path = path.join(__dirname, 'coin.json');
+  fs.writeFile(file_path, JSON.stringify(updated_coin_data), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.status(200).send('Coin data updated successfully');
+  });
+});
+
+//Game State Endpoints
+app.get('/gamestate', (req, res) => {
+  const file_path = path.join(__dirname, 'gamestate.json');
+  res.sendFile(file_path, err => {
+    if (err) {
+      res.status(404).send('Game state file not found');
+    }
+  });
+});
+
+app.put('/gamestate', (req, res) => {
+  const updated_gamestate_data = req.body;
+  const file_path = path.join(__dirname, 'gamestate.json');
+  fs.writeFile(file_path, JSON.stringify(updated_gamestate_data), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.status(200).send('Game state updated successfully');
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
