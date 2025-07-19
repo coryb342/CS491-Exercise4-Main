1b438c189282fa46d0f734a15593c20740498f2c
Cory Bateman <coryb342@gmail.com>
2025-07-18 17:09:05 -0700

    Add board size and winning combinations

--- public/client.js
+++ public/client.js
@@ -2,7 +2,24 @@ import * as player from './player_module.js';
 import * as gamestate from './gamestate_module.js';
 
 const player_id = null;
+const board_size = 16;
+const winning_combinations = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+
+    [0, 5, 10, 15],
+    [3, 6, 9, 12]
+];
+
 let polling = null;
+
 
 /**
  * @typedef {Object} Player
